0e4d419245cb374e8c87d1a10dd9c2324ddce74c
Kishor Neupane <heyneupkishor@gmail.com>
2023-04-29 21:32:35 +0545

    card, clients, and menu completed

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,8 +1,8 @@
 // The following are the element selectors.
-const toggleButton = document.querySelector(".topnav__toggle-container> .topnav__toggle-button");
-const toggleIcon = document.querySelector(".topnav__toggle-container > .topnav__toggle-button > .topnav__toggle-icon");
-const menuContainer = document.querySelector('.topnav > .topnav__container .topnav__menu-container');
-const topnav = document.querySelector(".topnav");
+const toggleButton = document.querySelector(".top-nav__toggle-container> .top-nav__toggle-button");
+const toggleIcon = document.querySelector(".top-nav__toggle-container > .top-nav__toggle-button > .top-nav__toggle-icon");
+const menuContainer = document.querySelector('.top-nav > .top-nav__container .top-nav__menu-container');
+const topNav = document.querySelector(".top-nav");
 
 // Make variable in Global Scope to save scroll position of user.
 let scrollPosition = 0;
@@ -11,18 +11,18 @@ let scrollPosition = 0;
 toggleButton.addEventListener('click', () => {
 
 	// Toggle the class lists when click is done.
-	menuContainer.classList.toggle("topnav__menu-container--open");
+	menuContainer.classList.toggle("top-nav__menu-container--open");
 	toggleIcon.classList.toggle("toggle-icon--clicked");
 
 	// Check if the navigation bar is expanded.
-	if (menuContainer.classList.contains("topnav__menu-container--open")) {
+	if (menuContainer.classList.contains("top-nav__menu-container--open")) {
 		// Remove the scrollbar of the body.
 		container.style.overflowY = 'hidden';
 		// Make the page unscrollable, but show a scrollbar.
-		topnav.style.overflowY = "scroll"
+		topNav.style.overflowY = "scroll";
 	} else {
-		// Remove the scrollbar of the topnav.
-		topnav.style.overflowY = "hidden";
+		// Remove the scrollbar of the top-nav.
+		topNav.style.overflowY = "hidden";
 		// Make the page scrollable, also show the old scrollbar.
 		document.body.style.overflowY = "scroll";
 	}
