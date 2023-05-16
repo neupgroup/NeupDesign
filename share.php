838992858273f8e5077ff19fffc46e9feaffb0e5
Kishor Neupane <heyneupkishor@gmail.com>
2023-05-16 19:08:17 +0545

    changes

--- share.php
+++ share.php
@@ -2,40 +2,39 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Tourio | Travellers Companion</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/neupgroup/neupdesign/web/main.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tourio | Travellers Companion</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/neupgroup/neupdesign/web/main.css">
 </head>
 
 <body>
 
-	<?php require "./static/comp_header.php" ?>
+    <?php require "./static/comp_header.php" ?>
 
-	<main>
+    <main>
 
-    <?php
-    $l = $_GET['l'];
-    IF $l == ""
-    ?>
+        <?php
+        ?>
 
 
-    <section class="section section-no-access">
-        <div class="container">
-            
-                    <h1>Please Refer to the Menu for Now.</h1>
-                    <span>We're under development. The site will be live under 24 hours. We are extremely sorry for the inconvenice faced by you.</span>
-                    <br>
-                    <br>
-                    <span>Thank you for Scanning us. Thank you for being with us.</span>
-            
-        </div>
-    </section>
+        <section class="section section-no-access">
+            <div class="container">
 
-	</main>
+                <h1>Please Refer to the Menu for Now.</h1>
+                <span>We're under development. The site will be live under 24 hours. We are extremely sorry for the
+                    inconvenice faced by you.</span>
+                <br>
+                <br>
+                <span>Thank you for Scanning us. Thank you for being with us.</span>
 
-	<?php require "./static/comp_footer.php" ?>
+            </div>
+        </section>
+
+    </main>
+
+    <?php require "./static/comp_footer.php" ?>
 
 </body>
 
